efd9070d3ef19b53908d0f3fb2cb4cefb4ad6799
Slava Bilokur <vbilokur@danavero.com>
2021-05-16 01:03:39 +0300

    package name refactoring

---
+++ shared/build/generated/sqldelight/code/KMMTDB/commonMain/tk/svsq/mobile/TBreed.kt
@@ -0,0 +1,19 @@
+package tk.svsq.mobile
+
+import kotlin.Boolean
+import kotlin.Long
+import kotlin.String
+
+public data class TBreed(
+  public val id: Long,
+  public val name: String,
+  public val favorite: Boolean
+) {
+  public override fun toString(): String = """
+  |TBreed [
+  |  id: $id
+  |  name: $name
+  |  favorite: $favorite
+  |]
+  """.trimMargin()
+}
